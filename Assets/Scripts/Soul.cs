3a9e08a14ee1358931b643b968bcf3728fc511f2
Yuzhou Feng <gghg1989@gmail.com>
2021-01-30 03:37:30 -0500

    Add tilemap, basic player control and soul collection.

---
+++ Assets/Scripts/Soul.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soul : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collisionObject)
+    {
+        if (collisionObject.gameObject.name == "player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
