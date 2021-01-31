6202da0389bb1309558597b7b102881d1d51f2ab
Precious <pnavarro0996@gmail.com>
2021-01-31 14:11:59 -0500

    logic for dead hero and logic in player class

---
+++ Assets/Scripts/DeadHeroClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadHeroClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnColliderEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            PlayerClass player = FindObjectOfType<PlayerClass>();
+
+            if(player.soulCount == player.maxSouls)
+            {
+                //Revive Animation
+                //LoadScene();
+            }
+        }
+    }
+}
